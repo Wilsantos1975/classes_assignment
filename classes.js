4525a91da2d45544f24670b744b435378ab280de
Brandon Brown <brandonb0206@gmail.com>
2019-10-21 20:20:55 -0400

    question 1a

---
+++ classes.js
@@ -0,0 +1,13 @@
+//Question 1a.
+class Person {
+    constructor(firstName, lastName, middleName){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.middleName = middleName
+    }
+}
+
+let brandon = new Person("Brandon", "Brown", "O'neil")
+let wil = new Person("Wil", "Fausto", "Santos")
+console.log(brandon.firstName)
+//Question 1b.
